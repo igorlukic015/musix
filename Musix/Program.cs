a1954209004b6c41bfbc17826599d6ac48623e7a
igorlukic015 <igor.lukic015@gmail.com>
2026-03-29 18:33:13 +0200

    Filter out windows default session, and filter out non active sessions.

--- Musix/Program.cs
+++ Musix/Program.cs
@@ -1,12 +1,22 @@
 using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System.Diagnostics;
 
 MMDeviceEnumerator enumerator = new();
 MMDevice device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-Console.WriteLine(device.FriendlyName);
+Console.WriteLine($"default audio endpoint name: {device.FriendlyName}");
 
 SessionCollection sessions = device.AudioSessionManager.Sessions;
 for (int i = 0; i < sessions.Count; i++)
 {
     AudioSessionControl session = sessions[i];
-    Console.WriteLine(session.GetProcessID);
+
+    if (session.GetProcessID == 0 || session.State != AudioSessionState.AudioSessionStateActive)
+    {
+        continue;
+    }
+
+    string processName = Process.GetProcessById((int)session.GetProcessID).ProcessName;
+
+    Console.WriteLine($"Process: {processName}");
 }
