bea70e100e8959bb48593570750f22b9271d5994
igorlukic015 <igor.lukic015@gmail.com>
2026-03-29 18:25:55 +0200

    Print audio sessions process ids

--- Musix/Program.cs
+++ Musix/Program.cs
@@ -3,3 +3,10 @@ using NAudio.CoreAudioApi;
 MMDeviceEnumerator enumerator = new();
 MMDevice device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
 Console.WriteLine(device.FriendlyName);
+
+SessionCollection sessions = device.AudioSessionManager.Sessions;
+for (int i = 0; i < sessions.Count; i++)
+{
+    AudioSessionControl session = sessions[i];
+    Console.WriteLine(session.GetProcessID);
+}
